5573554c5a1b2ca9eb6eb962b2da6605bfb89cce
周睿 <zrufo747@outlook.com>
2025-09-18 16:59:43 +0800

    fmt code

--- src/aarch64/uspace.rs
+++ src/aarch64/uspace.rs
@@ -6,63 +6,13 @@ use core::{
 };
 
 use aarch64_cpu::registers::ESR_EL1;
-
 use memory_addr::VirtAddr;
 
 use crate::{
-    trap::{ExceptionKind, ReturnReason},
     TrapFrame,
     aarch64::trap::TrapKind,
+    trap::{ExceptionKind, ReturnReason},
 };
-
-/// Context to enter user space.
-pub struct UserContext(TrapFrame);
-
-impl UserContext {
-    /// Creates an empty context with all registers set to zero.
-    pub const fn empty() -> Self {
-        Self(TrapFrame::new())
-    }
-
-    /// Creates a new context with the given entry point, user stack pointer,
-    /// and the argument.
-    pub fn new(entry: usize, ustack_top: VirtAddr, arg0: usize) -> Self {
-        use aarch64_cpu::registers::SPSR_EL1;
-        let mut regs = [0; 31];
-        regs[0] = arg0 as _;
-        Self(TrapFrame {
-            r: regs,
-            usp: ustack_top.as_usize() as _,
-            tpidr: 0,
-            elr: entry as _,
-            spsr: (SPSR_EL1::M::EL0t
-                + SPSR_EL1::D::Masked
-                + SPSR_EL1::A::Masked
-                + SPSR_EL1::I::Unmasked
-                + SPSR_EL1::F::Masked)
-                .value,
-        })
-    }
-
-    /// Creates a new context from the given [`TrapFrame`].
-    pub const fn from(trap_frame: &TrapFrame) -> Self {
-        Self(*trap_frame)
-    }
-}
-
-impl Deref for UserContext {
-    type Target = TrapFrame;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for UserContext {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
 
 #[derive(Debug, Clone, Copy)]
 pub struct ExceptionInfo {
