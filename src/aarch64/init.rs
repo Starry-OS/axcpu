6859bb07c8fd8e11a4763a372705e96e03e27513
yining wang <103822966+yeanwang666@users.noreply.github.com>
2025-11-11 02:15:39 +0000

    aarch64: default should disable SPAN Signed-off-by: Weikang Guo guoweikang@kylinos.cn

--- src/aarch64/init.rs
+++ src/aarch64/init.rs
@@ -91,6 +91,7 @@ pub unsafe fn init_mmu(root_paddr: PhysAddr) {
 
     // Enable the MMU and turn on I-cache and D-cache
     SCTLR_EL1.modify(SCTLR_EL1::M::Enable + SCTLR_EL1::C::Cacheable + SCTLR_EL1::I::Cacheable);
+    // Disable SPAN
     barrier::isb(barrier::SY);
 }
 
